913981bead4718e38292b3591e91d27fd5ffefd7
jettechnologies <tomesitime04@gmail.com>
2025-08-14 16:04:53 +0100

    feat: reduced the background of student loading page

--- src/app/student/loading.tsx
+++ src/app/student/loading.tsx
@@ -4,7 +4,7 @@ import { Spinner } from "@/components/shared"
 
 export default function Loading() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-white px-4">
+		<div className="flex flex-col items-center justify-center min-h-screen bg-white/20 px-4">
 			{/* SVG Spinner */}
 			<Spinner />
 
